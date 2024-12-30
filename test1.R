b5b39ce871d927d7ebef6e0a04d9ec84a5170a2e
ingibjorg257 <87853510+ingibjorg257@users.noreply.github.com>
2024-12-30 10:42:22 +0000

    Update test1.R
    Change age filter

--- test1.R
+++ test1.R
@@ -6,7 +6,7 @@ print("Hello dear Imba!")
 gss_cat
 
 TESTtable<-gss_cat %>%
-  filter(age >= 30)%>%
+  filter(age <= 29)%>%
   group_by(marital) %>%
   summarise(meant_tv_hour= mean(tvhours, na.rm=T))
 
